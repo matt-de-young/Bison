43f7ba0c517c74f8c8e318636dc9ad65d7e18446
Matt Geiger <Mail@MattGeiger.com>
2014-02-08 09:29:44 -0500

    Added unit testing
    Part of the main file now includes some unit tests that can be switched
    on or off.

--- lib/buffalo.rb
+++ lib/buffalo.rb
@@ -1,13 +1,48 @@
 require_relative 'buffalo/symbols'
 require_relative 'buffalo/token'
 
-symbols = Symbols.new(0) # crate new hash for each scope (function)
-symbols.add("num", "name", "value")
+def unitTests
 
-puts "name already exists!" if symbols.add("string", "name", "value") == false
+	# Test creation of symbol table & addition of a token
+	symbols = Symbols.new(0) # crate new hash for each scope (0 in this case)
+	symbols.add("num", "name", "value")
+	if symbols.has "name"
+		puts "Table creation:      Passed"
+	else
+		puts "Table creation:      Failed"
+	end
 
-puts "test exists" if symbols.has("test")
-puts "test does not exist" unless symbols.has("test")
+	# Test collision detection for adding tokens
+	unless symbols.add("string", "name", "value")
+		puts "Collision detection: Passed" 
+	else
+		puts "Collision detection: Failed" 
+	end
 
-newToken = symbols.get("name")
-newToken.display_details
+	# Test token detection
+	unless symbols.has("test")
+		puts "Token detection:     Passed" 
+	else
+		puts "Token detection:     Failed" 
+	end
+
+	# Test token retreval
+	newToken = symbols.get("name")
+	unless newToken == nil
+		puts "Token retreval:      Passed" 
+	else
+		puts "Token retreval:      Failed" 
+	end
+
+	# Test token type detection
+	if symbols.get("name").type == "num" && symbols.get("name").type != "string"
+		puts "Type detection:      Passed" 
+	else
+		puts "Type detection:      Failed"
+	end
+
+end
+
+# Run unit tests
+unitTests
+puts ""
