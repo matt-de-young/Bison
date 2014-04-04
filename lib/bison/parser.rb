64b4e567e271e35f3a736ac3a0b692e41cd29382
Matt Geiger <Mail@MattGeiger.com>
2014-04-04 17:47:04 -0400

    Changed the way the parse table is created
    The parse table will be encoded by retrieving the corresponding code
    for each token.

--- lib/bison/parser.rb
+++ lib/bison/parser.rb
@@ -79,61 +79,24 @@ class Parser
 		words["*"] = 47
 		words["/"] = 48
 		words["MOD"] = 49
-		words["."] = 50
-		words["("] = 51
-		words[")"] = 52
-		words[">"] = 53
-		words[">="] = 54
-		words["="] = 55
-		words["<="] = 56
-		words["<"] = 57
-		words["<>"] = 58
-		words["ID"] = 59
+		words["("] = 50
+		words[")"] = 51
+		words[">"] = 52
+		words[">="] = 53
+		words["="] = 54
+		words["<="] = 55
+		words["<"] = 56
+		words["<>"] = 57
+		words["ID"] = 58
+		words[":="] = 59
+		words[";"] = 60
 
 		# Parse table
-		table[0][30] = ['shift', 1]
-		table[0][29] = ['reduce', 3]
-		table[0][1] = ['goto', 2]
-		table[0][2] = ['goto', 3]
+		table[0][words["DECLARE"]] = ['shift', 1]
+		table[0][words["BEGIN"]] = ['reduce', 3]
+		table[0][words["block"]] = ['goto', 2]
+		table[0][words["declarations"]] = ['goto', 3]
 
-		table[1][59] = ['shift', 4]
-		table[1][29] = ['reduce', 5]
-		table[1][3] = ['goto', 5]
-
-		table[2][44] = ['accept', ]
-
-		table[3][29] = ['shift', 6]
-		table[3][50] = ['error', ]
-		table[3][10] = ['goto', 7]
-
-		table[4][23] = ['shift', 8]
-		table[4][28] = ['shift', 9]
-		table[4][24] = ['shift', 10]
-		table[4][25] = ['shift', 11]
-		table[4][26] = ['shift', 12]
-		table[4][27] = ['shift', 13]
-		table[4][50] = ['error', ]
-		table[4][5] = ['goto', 14]
-		table[4][6] = ['goto', 15]
-		table[4][9] = ['goto', 16]
-
-		table[5][50] = ['reduce', 2]
-
-		table[6][59] = ['shift', 17]
-		table[6][29] = ['shift', 6]
-		table[6][38] = ['shift', 18]
-		table[6][41] = ['shift', 19]
-		table[6][40] = ['shift', 20]
-		table[6][42] = ['shift', 21]
-		table[6][32] = ['shift', 22]
-		table[6][34] = ['shift', 23]
-		table[6][43] = ['shift', 24]
-		table[6][50] = ['error', ]
-		table[6][10] = ['goto', 25]
-		table[6][11] = ['goto', 26]
-		table[6][12] = ['goto', 27]
-		table[6][13] = ['goto', 28]
-		table[6][14] = ['goto', 29]
 
 
 	end
